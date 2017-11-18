4031aaed0acc1f9631b42fa78dd1498eeb5dcd28
WheelsOnPavement <sam.hosmer@gmail.com>
2017-11-18 15:27:49 -0800

    Cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,27 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
 import javax.security.auth.login.LoginException;
-import java.util.EventListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
 
 public class Main {
+
     public static void main(String[] args) throws LoginException, InterruptedException, RateLimitedException {
-        JDA jda = new JDABuilder(AccountType.BOT).setToken("token").buildBlocking();
+
+        // generate a config file
+        Properties prop = new Properties();
+        OutputStream output = null;
+
+        // load token from config
+        Settings settings = new Settings();
+        System.out.println(settings.getToken());
+
+        //JDA jda = new JDABuilder(AccountType.BOT).setToken(settings.getToken()).buildBlocking();
         System.out.println("Hello world");
     }
 }
