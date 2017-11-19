365f1a29c39a0a1b944edab834ee6e2a6c8f571f
WheelsOnPavement <sam.hosmer@gmail.com>
2017-11-18 18:32:26 -0800

    TrackScheduler

--- src/main/java/BidenBot/BidenBot.java
+++ src/main/java/BidenBot/BidenBot.java
@@ -1,6 +1,23 @@
 package BidenBot;
 
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
 public class BidenBot {
 
-    
+    public final AudioPlayer player;
+    public final TrackScheduler scheduler;
+
+    public BidenBot(AudioPlayerManager manager) {
+        player = manager.createPlayer();
+        scheduler = new TrackScheduler(player);
+        player.addListener(scheduler);
+    }
+
+    /**
+     *
+     * @return Wrapper around AudioPlayer to ues it as an AudioSendHandler.
+     */
+    public AudioPlayerSendHandler getSendHandler() { return new AudioPlayerSendHandler(player); }
+
 }
