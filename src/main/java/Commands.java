72bd0f3f1905b61f9de8d7124bb6902c4c0345e4
Alexander Wang <alexander.wang2001@gmail.com>
2017-11-18 16:30:03 -0800

    Added Cmd

---
+++ src/main/java/Commands.java
@@ -0,0 +1,29 @@
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class Commands extends ListenerAdapter{
+    private static final String CMD_PREFIX = "!";
+
+    //@Override
+    public void onMessageRecieved(MessageReceivedEvent e){
+        String[] command = e.getMessage().getContent().split(" ");
+
+        Message msg = e.getMessage();
+        MessageChannel channel = e.getTextChannel();
+        User user = e.getAuthor();
+
+        if(!command[0].startsWith(CMD_PREFIX)){
+            return;
+        }else if(command[0].equalsIgnoreCase("!ping")){
+
+            String returnMsg = "Pong!";
+            channel.sendMessage(returnMsg).queue();
+
+        }
+
+    }
+
+}
